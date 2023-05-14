193dcfdfa3afec8f318debef96dcf4b1e29a706d
Elvis-codeur <83865024+Elvis-codeur@users.noreply.github.com>
2023-05-14 20:52:40 +0100

    Commit 3 du 14-05-2023

--- pendule/test_non_lineaire_1.py
+++ pendule/test_non_lineaire_1.py
@@ -1,19 +1,32 @@
 import numpy as np
 
-DT = 0.1
+DT = 0.01
 
 
 def f(X):
     return np.array([
-        X[0] - np.sin(X[0]) - X[1],
-        X[1]
+        2*X[0] + 4*X[1],
+        7*X[0] - 7*X[1]
     ])
 
 
 def df(X):
     return np.array([
-        [1 - np.cos(X[0]), -1],
-        [0, 1]
+        [2, 4],
+        [7, -7]
+    ])
+
+
+def f_sin(X):
+    return np.array([
+        DT*np.sin(X[0]) + X[1],
+        X[1]
+    ])
+
+def df_sin(X):
+    return np.array([
+        [DT*np.cos(X[0]), 1],
+        [0,1]
     ])
 
 
@@ -25,6 +38,7 @@ def newtonRaphson(X0, f, df, n_iteration):
         i += 1
     return X
 
+
 if __name__ == "__main__":
-    X0 = np.array([1,1.1])
-    print(newtonRaphson(X0,f,df,10))
+    X0 = np.array([1,1 ])
+    print(newtonRaphson(X0, f_sin, df_sin, 10))
