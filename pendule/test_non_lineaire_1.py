610c78ac479d174dec5ac172e91a2fce452325b6
Elvis-codeur <83865024+Elvis-codeur@users.noreply.github.com>
2023-05-16 20:58:52 +0100

    Commit 1 du 16-05-2023

--- pendule/test_non_lineaire_1.py
+++ pendule/test_non_lineaire_1.py
@@ -1,4 +1,7 @@
 import numpy as np
+from scipy.integrate import odeint
+import matplotlib.pyplot as plt
+
 
 DT = 0.01
 
@@ -30,7 +33,9 @@ def df_sin(X):
     ])
 
 
-def newtonRaphson(X0, f, df, n_iteration):
+
+
+def newtonRaphson_matrice(X0, f, df, n_iteration):
     X = X0
     i = 0
     while i < n_iteration:
@@ -38,7 +43,66 @@ def newtonRaphson(X0, f, df, n_iteration):
         i += 1
     return X
 
+def newtonRaphson(X0, f, df, n_iteration):
+    X = X0
+    i = 0
+    while i < n_iteration:
+        X = X - f(X)/df(X)
+        i += 1
+    return X
+
+
+def model(y,t):
+    return np.sin(y)
+
+def solve(y0,dt,T):
+    yi = y0 
+
+    def f(x):
+        return x -dt*np.sin(x) - yi
+    
+    def df(x):
+        return 1 - dt*np.cos(x) 
+
+    
+    time = 0
+    t = [0]
+    s = [y0] 
+
+    while(time < T):
+        time += dt 
+        y0 = newtonRaphson(yi,f,df,10)
+        yi = y0 
+        t.append(time)
+        s.append(y0)
+
+    return t,s 
+
+
+    
+
+
+def odeint_result():
+    y0 = 5
+    # time points
+    t = np.arange(0,20,0.01)
+    # solve ODE
+    y = odeint(model,y0,t)
+    return t,y 
+
+def test_newtonRaphson():
+    def f(x):
+        return x - np.cos(x)
+    def df(x):
+        return 1 + np.sin(x)
+    
+    print(newtonRaphson(1,f,df,10))
 
 if __name__ == "__main__":
-    X0 = np.array([1,1 ])
-    print(newtonRaphson(X0, f_sin, df_sin, 10))
+    test_newtonRaphson()
+    t,s =solve(5,0.01,20)
+    ode_t, ode_s = odeint_result()
+    plt.plot(t,s,label = "Résultat de mon solveur")
+    plt.plot(ode_t,ode_s,label = "Résultat de odeint")
+    plt.legend()
+    plt.show()
