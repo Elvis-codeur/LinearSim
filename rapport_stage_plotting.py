be0d4a2e6d56418584fb994c3ab33f94ee956762
Elvis-codeur <83865024+Elvis-codeur@users.noreply.github.com>
2023-06-27 06:22:14 +0100

    Commmit 1 du 27-06-2023

---
+++ rapport_stage_plotting.py
@@ -0,0 +1,44 @@
+import numpy as np
+import pandas as pd
+import matplotlib.pyplot as plt
+from core import *
+
+
+def test_rapport():
+
+    filename = "C:\\Users\\Elvis\\Downloads\\data_step_1.csv"
+    df = pd.read_csv(filename, sep=",", names=["t", "s"], header=None)
+    print(df.head(5))
+
+    
+
+    Num = Polynome([6, ])
+    Den = Polynome([100, 14, 1])
+    H = SystemeLineaire(Num, Den)
+
+    t = df["t"]
+    C = np.zeros(Den.coeff.shape[0]-1)
+    C[0] = 1
+    X0 = np.zeros(Den.coeff.shape[0]-1)
+
+    def U_interne(x):
+        return np.array([1, 0])
+
+    srk4 = rk4(H, t, U_interne, C, X0)
+
+    fig, ax = plt.subplots()
+    fig.set_dpi(150)
+
+    ax.plot(t, srk4, label="Mon Solveur")
+    ax.plot(df["t"], df["s"], label="Matlab")
+
+    ax.set_xlabel("temps (s)")
+    ax.set_ylabel("Réponse du sytème")
+    #fig.suptitle(r"H(p) = \frac{6}{100p^2 + 14p + 1}")
+
+    ax.legend()
+    plt.show()
+
+
+if __name__ == "__main__":
+    test_rapport()
