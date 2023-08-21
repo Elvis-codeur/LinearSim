6788857a83a61eeff46a1b959c3b220931cebb0d
Elvis-codeur <83865024+Elvis-codeur@users.noreply.github.com>
2023-08-21 20:47:35 +0100

    Commit 2 du 21-08-2023

---
+++ EDP/equation_chaleur_1d/equation_chaleur_1d_version_rapide.py
@@ -0,0 +1,25 @@
+import numpy as np 
+import matplotlib.pyplot as plt 
+
+NX = 100
+NT = 1000
+L = 1
+Time = 0.1 
+k = 0.5
+dt = Time/NT
+dx = L/(NX - 1)
+
+x =  np.linspace(0.0,1.0,NX)
+T = np.sin(2*np.pi*x)
+RHS = np.zeros((NX))
+
+for n in range(0,NT):
+    RHS[1:-1] = dt*k*(T[:-2] - 2*T[1:-1] + T[2:])/(dx**2)
+    T += RHS 
+
+    if n % 100 == 0:
+        plt.plot(x,T,label = "%1.2f"%(n*dt))
+
+plt.legend()
+plt.show() 
+    
